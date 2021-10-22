a33a24b2952cff0bb38a59dd78ff2ed428d9c398
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-10-22 15:22:06 +0530

    Update project settings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    int sceneCount;
 
+    void Awake()
+    {
+        sceneCount = SceneManager.sceneCountInBuildSettings;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadGameScene(int index)
     {
-
+        SceneManager.LoadScene(index);
     }
+
+    public void ResetScene()
+    {
+        LoadGameScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextScene()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (sceneIndex >= sceneCount)
+        {
+            sceneIndex = 0;
+        }
+        LoadGameScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMainMenu()
+    {
+        LoadGameScene(0);
+    }
+
 }
